e6efee08e62f564f8cdf0a27dcb350649e4ab0ab
Citlalli DMG <citlallidmg@gmail.com>
2018-12-03 20:20:07 -0600

    Updates

---
+++ precache-manifest.7a0a5af747c7b0045cedef67713caf8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dd54f853a1bffb0e9979f8146268af3",
+    "url": "/talent-fest-grin/static/media/google-logo-icon-PNG-Transparent-Background.0dd54f85.png"
+  },
+  {
+    "revision": "68317b979c4d4ccd2703",
+    "url": "/talent-fest-grin/static/js/runtime~main.68317b97.js"
+  },
+  {
+    "revision": "ef0ae71cc3e307b6a5fd",
+    "url": "/talent-fest-grin/static/js/main.ef0ae71c.chunk.js"
+  },
+  {
+    "revision": "7f2605c239937c0574cd",
+    "url": "/talent-fest-grin/static/js/1.7f2605c2.chunk.js"
+  },
+  {
+    "revision": "ef0ae71cc3e307b6a5fd",
+    "url": "/talent-fest-grin/static/css/main.26c07737.chunk.css"
+  },
+  {
+    "revision": "d5e39fb3e694bfc8d27d09c2bcaca8cc",
+    "url": "/talent-fest-grin/index.html"
+  }
+];
